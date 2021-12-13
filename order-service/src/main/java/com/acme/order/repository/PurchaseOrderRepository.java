0ee891c859bc4f3bc78024562c21b87545406a45
Pettisani Diego <diego.pettisani@msccrociere.it>
2021-12-13 18:53:08 +0100

    Removed order service folder

--- order-service/src/main/java/com/acme/order/repository/PurchaseOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.acme.order.controller.model.PurchaseOrder;
-
-public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long>{
-
-}
---
+++ purchase-order/README.md
@@ -0,0 +1,2 @@
+# spring-boot-dynatrace-opentelemetry-poc
+A POC for testing the Dynatrace integration with OpenTelemetry
